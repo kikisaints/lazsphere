545e42409bfe115898d5a737a4d1e749cef0a1d5
Kiki Saintonge <matrixtunnel@gmail.com>
2022-10-29 13:49:39 -0700

    basic movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,6 +15,9 @@ public class PlayerMovement : NetworkBehaviour
     private float rotateSpeed = 7f;
 
     [SerializeField]
+    private float maxSpeed = 25f;
+
+    [SerializeField]
     private Transform cameraLocation;
 
     [SerializeField]
@@ -22,7 +25,15 @@ public class PlayerMovement : NetworkBehaviour
 
     private Vector2 m_OldlookRotation;
 
+    private Rigidbody physicsBody;
+
     private NetworkVariable<Quaternion> networkedRotation = new NetworkVariable<Quaternion>();
+    private NetworkVariable<Vector3> networkedVelocity = new NetworkVariable<Vector3>();
+
+    private void Start()
+    {
+        physicsBody = this.GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
@@ -51,10 +62,12 @@ public class PlayerMovement : NetworkBehaviour
 
         tempVec.x += Input.GetAxis("Mouse X") * sensitivity;
         tempVec.y -= Input.GetAxis("Mouse Y") * sensitivity;
-        tempVec.x = Mathf.Repeat(tempVec.x, 360);
+        tempVec.x = Mathf.Repeat(tempVec.x, 359);
         tempVec.y = Mathf.Clamp(tempVec.y, -maxYAngle, maxYAngle);
 
-        UpdateRotationServerRpc(Quaternion.Euler(tempVec.y, tempVec.x, 0));
+        UpdateMovementServerRpc(Quaternion.Euler(tempVec.y, tempVec.x, 0), 
+            this.transform.forward * Input.GetAxis("Vertical") * maxSpeed);
+
         m_OldlookRotation = tempVec;
 
         if (Input.GetMouseButtonDown(0))
@@ -64,11 +77,13 @@ public class PlayerMovement : NetworkBehaviour
     private void UpdateServer()
     {
         this.transform.rotation = Quaternion.Lerp(this.transform.rotation, networkedRotation.Value, Time.deltaTime * rotateSpeed);
+        physicsBody.velocity = networkedVelocity.Value;
     }
 
     [ServerRpc]
-    private void UpdateRotationServerRpc(Quaternion rot)
+    private void UpdateMovementServerRpc(Quaternion rot, Vector3 vel)
     {
-        networkedRotation.Value = rot;        
+        networkedRotation.Value = rot;
+        networkedVelocity.Value = vel;
     }
 }
